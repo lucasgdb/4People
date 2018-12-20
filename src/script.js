190b669978c78c3c911f03b2e61fa5af774d4e97
Lucas Bittencourt <lucasnaja0@gmail.com>
2018-12-20 19:36:55 -0200

    first commit

---
+++ src/script.js
@@ -0,0 +1,23 @@
+const btns = document.querySelectorAll('.btn'),
+  btnTop = document.querySelectorAll('.btnTop');
+
+for (let i = 0; i < btns.length; i++)
+  btns[i].onclick = () => {
+    const smenu = btns[i].parentElement.querySelector('.smenu');
+    for (let j = 0; j < btns.length; j++)
+      if (btns[i] !== btns[j])
+        btns[j].parentElement.querySelector('.smenu').className = 'smenu'
+
+    if (smenu.className === 'smenu actived')
+      smenu.className = 'smenu'
+    else smenu.className = 'smenu actived'
+  }
+
+for (let i = 0; i < btnTop.length; i++)
+  btnTop[i].onclick = () => {
+    const menu = btnTop[i].parentElement.querySelector('.menu');
+
+    if (menu.className === 'menu actived')
+      menu.className = 'menu'
+    else menu.className = 'menu actived'
+  }
