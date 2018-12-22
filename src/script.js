a26c3e87821c354816eeb83e17aca375fc43c065
Lucas Bittencourt <lucasnaja0@gmail.com>
2018-12-22 02:47:26 -0200

    fix

--- src/script.js
+++ src/script.js
@@ -1,22 +1,32 @@
 const btns = document.querySelectorAll('.btn'),
-  btnTop = document.querySelectorAll('.btnTop');
+  btnTop = document.querySelectorAll('.btnTop'),
+  menu = document.querySelector('.containerMenus'),
+  nav = document.querySelector('.leftNav');
 
 for (let i = 0; i < btns.length; i++)
   btns[i].onclick = () => {
     const smenu = btns[i].parentElement.querySelector('.smenu');
-    if (smenu !== null) {
-      if (smenu.style.maxHeight === '100em')
-        smenu.style.maxHeight = '0em'
-      else smenu.style.maxHeight = '100em'
-    }
+    for (let j = 0; j < btns.length; j++)
+      if (btns[i] !== btns[j])
+        btns[j].parentElement.querySelector('.smenu').className = 'smenu'
+
+    if (smenu.className === 'smenu actived')
+      smenu.className = 'smenu'
+    else smenu.className = 'smenu actived'
   }
 
 for (let i = 0; i < btnTop.length; i++)
   btnTop[i].onclick = () => {
     const menu = btnTop[i].parentElement.querySelector('.menu');
-    if (menu !== null) {
-      if (menu.style.maxHeight === '0em')
-        menu.style.maxHeight = '100em'
-      else menu.style.maxHeight = '0em'
-    }
+
+    if (menu.className === 'menu actived')
+      menu.className = 'menu'
+    else menu.className = 'menu actived'
   }
+
+menu.onclick = () => {
+  if (nav.style.display === 'block')
+    nav.style.display = 'none'
+  else nav.style.display = 'block'
+}
+
