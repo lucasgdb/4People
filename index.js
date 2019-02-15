8e0a6cc4820d888ae1cf7856e782384a2f39f142
Lucas Bittencourt <lucasnaja0@gmail.com>
2019-02-15 17:19:55 -0200

    nodejs added to the project

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const router = express.Router()
+const port = process.env.PORT || 5501
+
+app.set('port', port)
+
+app.use(express.static('public'))
+
+app.use('/', (req, res) => {
+  res.send('<meta http-equiv="refresh" content="0; url=./" />')
+})
+
+app.listen(port, () => console.log('Server initialized'))
