3c8c082200271b742ca3d31d91e2ac90c420a72e
Lucas Bittencourt <lucasnaja0@gmail.com>
2019-04-10 01:27:08 -0300

    added nicks generator

---
+++ assets/algorithms/nickGenerator.js
@@ -0,0 +1,55 @@
+const nicks = [
+    'off',
+    'Desu',
+    'Sasha',
+    'Das',
+    'Mann',
+    'Storm',
+    'Elvis',
+    'Sugar',
+    'Saint',
+    'Shadow',
+    'Yoda',
+    'Crush',
+    'Hokage',
+    'Smash',
+    'Jefferson',
+    'Helssing',
+]
+const sufixNicks = [
+    'OfChaos',
+    'Senpai',
+    'OfFire',
+    'vski',
+    'litz',
+    'Ajax',
+    'JS',
+    'kun',
+    'Sama',
+    'Seiya'
+]
+const prefixNicks = [
+    'General',
+    'Smash',
+    'Stein',
+    'boy',
+    'Van',
+    'gai',
+    'Cruiser',
+    '4',
+]
+
+function generateNicks(name, option) {
+    //var randomName = faker.name.findName()
+    if (option == '2') {
+        return name += nicks[parseInt(Math.random() * nicks.length)]
+    } else if (option == '1') {
+        return name += nicks[parseInt(Math.random() * nicks.length)]
+    } else if (option == '3') {
+        return prefixNicks[parseInt(Math.random() * prefixNicks.length)] += nicks[parseInt(Math.random()) * nicks.length] += sufixNicks[parseInt(Math.random() * sufixNicks.length)]
+    } else if (option == '4') {
+        return prefixNicks[parseInt(Math.random() * prefixNicks.length)] += name
+    } else if (option == '5') {
+        return name += sufixNicks[parseInt(Math.random() * prefixNicks.length)]
+    }
+}
---
+++ computacao/geradores/gerador_de_nicks/src/index.js
@@ -0,0 +1,23 @@
+M.FormSelect.init(document.querySelectorAll('select'))
+const txtName = document.querySelector('#name')
+const txtResult = document.querySelectorAll('#result')
+const txtSelect = document.querySelector('select')
+const txtInput = document.querySelector('.areaText')
+
+function generate() {
+    if (txtSelect.value != '2' && txtSelect.value != '3') {
+        txtInput.className = 'col s12 areaText'
+        let nicks
+        nicks = generateNicks(txtName.value, txtSelect.value)
+        for (var i = 0; i <= txtResult.length; i++) {
+            txtResult[i].innerHTML = `${nicks}`
+        }
+    } else {
+        txtInput.className = 'col s12 areaText hide'
+        let nicks
+        nicks = generateNicks(txtName.value, txtSelect.value)
+        for (var i = 0; i <= txtResult.length; i++) {
+            txtResult[i].innerHTML = `${nicks}`
+        }
+    }
+}
