0cf4efd801e7451e72b30918cf90b6a4c59897de
Lucas Bittencourt <lucasnaja0@gmail.com>
2019-06-01 14:43:24 -0300

    changes

--- assets/components/logo.php
+++ assets/components/logo.php
@@ -1,8 +1,8 @@
 <li>
     <div class="user-view">
-        <div class="background grey lighten-3"></div>
+        <div class="background grey lighten-2"></div>
         <img class="circle" src="<?= $return ?>/images/logo.png">
-        <a><span class="black-text name">4People</span></a>
-        <a href="https://github.com/LucasNaja/4People" target="_blank" rel="noopener noreferrer"><span class="black-text email">Projeto de TCC - Etec ABS</span></a>
+        <a><span class="name">4People</span></a>
+        <a href="https://github.com/LucasNaja/4People" target="_blank" rel="noopener noreferrer nofollow"><span class="email">Projeto de TCC - Etec ABS »</span></a>
     </div>
 </li>
