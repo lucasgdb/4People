665f147924da97e1e6e8417798507965b7fc0e54
lucasnaja <lucasnaja0@gmail.com>
2019-07-08 23:43:55 -0300

    all date calculator algorithms were made

---
+++ assets/algorithms/dates_calculator/addInDate.js
@@ -0,0 +1,23 @@
+const addDays = (date, days) => {
+	const newDate = new Date(date)
+	newDate.setDate(newDate.getDate() + days)
+	return newDate
+}
+
+const addWeeks = (date, weeks) => {
+	const newDate = new Date(date)
+	newDate.setDate(newDate.getDate() + weeks * 7)
+	return newDate
+}
+
+const addMonths = (date, months) => {
+	const newDate = new Date(date)
+	newDate.setMonth(newDate.getMonth() + months)
+	return newDate
+}
+
+const addYears = (date, years) => {
+	const newDate = new Date(date)
+	newDate.setFullYear(newDate.getFullYear() + years)
+	return newDate
+}
