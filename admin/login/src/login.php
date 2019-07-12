d65080097144ecd22709ed6e2cd4cf909aa97f56
lucasnaja <lucasnaja0@gmail.com>
2019-07-12 19:59:21 -0300

    improved login code

--- admin/login/src/login.php
+++ admin/login/src/login.php
@@ -1,23 +1,20 @@
 <?php
 try {
 	include_once('../../../assets/connection.php');
-	include_once('../../php_codes/cript.php');
+	include_once('../../painel_administrativo/src/MD5.php');
 
 	$user_nickname = filter_input(INPUT_POST, 'user_nickname', FILTER_DEFAULT);
 	$user_password = cript(filter_input(INPUT_POST, 'user_password', FILTER_DEFAULT));
 
-	$sql = $database->prepare("SELECT user_name, user_email, user_image FROM users WHERE user_nickname=:user_nickname and user_password=:user_password");
+	$sql = $database->prepare("SELECT user_name, user_image FROM users WHERE user_nickname=:user_nickname and user_password=:user_password");
 
 	$sql->bindValue(":user_nickname", $user_nickname);
 	$sql->bindValue(":user_password", $user_password);
 
-	$sql->execute();
-
-	if ($sql->rowCount()) {
+	if ($sql->execute() && $sql->rowCount()) {
 		session_start();
 
 		$data = $sql->fetchAll()[0];
-		$_SESSION['logged']['email'] = $user_email;
 		$_SESSION['logged']['name'] = $data['user_name'];
 		$_SESSION['logged']['image'] = $data['user_image'];
 		header('Location: ../../painel_administrativo/');
