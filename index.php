a2ad508db5eb2764e0b6359ccec5592ecfe89cd5
lucasnaja <lucasnaja0@gmail.com>
2019-07-28 23:46:15 -0300

    removed other icon colors from slider

--- index.php
+++ index.php
@@ -56,14 +56,14 @@
 
 						<li class="grey lighten-3">
 							<div class="caption right-align">
-								<h3 class="dark grey-text text-darken-4">CÓDIGO ABERTO! <i style="top:10px" class="material-icons indigo-text text-darken-4 right small">code</i></h3>
+								<h3 class="dark grey-text text-darken-4">CÓDIGO ABERTO! <i style="top:10px" class="material-icons right small">code</i></h3>
 								<h5 class="light grey-text text-darken-4">O Projeto 4People é de Código Aberto para qualquer um estudar os algoritmos e até mesmo melhorá-los.</h5>
 							</div>
 						</li>
 
 						<li class="grey lighten-3">
 							<div class="caption right-align">
-								<h3 class="dark grey-text text-darken-4"><i style="top:10px" class="material-icons brown-text text-darken-2 right small">free_breakfast</i>O MAIS ATUALIZADO!</h3>
+								<h3 class="dark grey-text text-darken-4"><i style="top:10px" class="material-icons right small">free_breakfast</i>O MAIS ATUALIZADO!</h3>
 								<h5 class="light grey-text text-darken-4">O 4People possui as melhores ferramentas atualizadas. Tá sentindo falta de alguma? Por favor, nos envie uma <a href="./contato/">mensagem</a>.</h5>
 							</div>
 						</li>
