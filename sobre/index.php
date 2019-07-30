56139e277234b12866e35fca73c94382b5f60a1d
lucasnaja <lucasnaja0@gmail.com>
2019-07-30 10:04:27 -0300

    about page improvements

--- sobre/index.php
+++ sobre/index.php
@@ -7,6 +7,7 @@
 	<link rel="stylesheet" href="<?= $assets ?>/src/css/main.css">
 	<link rel="stylesheet" href="<?= $assets ?>/src/css/material-icons.css">
 	<link rel="stylesheet" href="<?= $assets ?>/src/css/bars.css">
+	<link rel="stylesheet" href="src/index.css">
 	<title>Sobre - 4People</title>
 	<?php include_once("$assets/components/meta_tags.php") ?>
 	<meta name="keywords" content="4people,4devs,pessoas,online,ferramentas,desenvolvedores,computacao,matematica,geradores,validadores,faker">
@@ -34,9 +35,9 @@
 				<h1 class="flow-text" style="margin:0 0 5px"><img src="<?= $assets ?>/images/icon.png" width="25" style="margin-right:10px" alt="Logo">Sobre</h1>
 
 				<label>4People é um site feito para ajudar estudantes, professores, programadores e pessoas em suas atividades diárias.</label>
-				<div class="divider mb-0"></div>
+				<div class="divider"></div>
 
-				<h5 class="center-align" style="position:relative"><i class="material-icons" style="position:absolute;left:0">info</i>Sobre o 4People</h5>
+				<h5 class="center-align mt-2" style="position:relative"><i class="material-icons" style="position:absolute;left:0">info</i>Sobre o 4People</h5>
 				<div class="divider"></div>
 				<p>
 					4People é um projeto de TCC que visa criar ferramentas para pessoas usarem no cotidiano e Programadores testarem seus Softwares. O 4People disponibiliza Ferramentas de Computação (Geradores, Validadores, Codificadores e Decodificadores, etc.) e Matemáticas (Calculadoras, Calculadoras de Área e Calculadoras de Datas).
@@ -44,36 +45,36 @@
 				</p>
 				<div class="divider"></div>
 
-				<h5 class="center-align" style="position:relative"><i class="material-icons" style="position:absolute;left:0">computer</i>Tecnologias usadas</h5>
+				<h5 class="center-align mt-2" style="position:relative"><i class="material-icons" style="position:absolute;left:0">computer</i>Tecnologias usadas</h5>
 				<div class="divider"></div>
 				<p>
 					<ul>
-						<li><img src="<?= $assets ?>/images/html5.svg" width="15" style="margin-right:5px" alt="">HTML5</li>
-						<li><img src="<?= $assets ?>/images/css3.png" style="margin-right:5px" alt="">CSS3</li>
-						<li>JavaScript</li>
-						<li>PHP</li>
-						<li>Materialize</li>
-						<li>Docker</li>
+						<li><img src="<?= $assets ?>/images/html5.svg" width="25" style="margin-right:5px;top:7px" alt="HTML5">HTML5</li>
+						<li><img src="<?= $assets ?>/images/css3.png" style="margin-right:5px;top:7px" alt="CSS3">CSS3</li>
+						<li><img src="<?= $assets ?>/images/javascript.png" style="margin-right:5px;top:7px" alt="JavaScript">JavaScript</li>
+						<li style="margin-top:7px"><img src="<?= $assets ?>/images/php.png" style="margin-right:5px;top:3px" alt="PHP">PHP</li>
+						<li style="margin-top:5px"><img src="<?= $assets ?>/images/materialize.png" style="margin-right:5px" alt="Materialize">Materialize</li>
+						<li style="margin-top:-5px"><img src="<?= $assets ?>/images/docker.png" style="margin-right:5px;top:6px" alt="Docker">Docker</li>
 					</ul>
 				</p>
 				<div class="divider"></div>
 
-				<h5 class="center-align" style="position:relative"><i class="material-icons" style="position:absolute;left:0">group</i>Equipe</h5>
+				<h5 class="center-align mt-2" style="position:relative"><i class="material-icons" style="position:absolute;left:0">group</i>Equipe</h5>
 				<div class="divider"></div>
 				<div class="row mb-0">
-					<div class="col s12 m3 mt-2">
+					<div class="col s6 m3 mt-2">
 						<img title="Lucas Bittencourt" class="circle responsive-img" src="<?= $assets ?>/images/lucas_bittencourt.jpg" alt="">
 					</div>
 
-					<div class="col s12 m3 mt-2">
+					<div class="col s6 m3 mt-2">
 						<img title="Jairo Arcy" class="circle responsive-img" src="<?= $assets ?>/images/jairo_arcy.jpeg" alt="">
 					</div>
 
-					<div class="col s12 m3 mt-2">
+					<div class="col s6 m3 mt-2">
 						<img title="Suzany Silva" class="circle responsive-img" src="<?= $assets ?>/images/suzany_silva.jpg" alt="">
 					</div>
 
-					<div class="col s12 m3 mt-2">
+					<div class="col s6 m3 mt-2">
 						<img title="Renan Mattos" class="circle responsive-img" src="<?= $assets ?>/images/renan_mattos.jpg" alt="">
 					</div>
 				</div>
