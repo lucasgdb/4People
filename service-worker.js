9c85297efc5da41f37096831537b8c91fa0049dc
lucasnaja <lucasnaja0@gmail.com>
2019-08-03 14:50:45 -0300

    service-worker fixes; offline page visual improved

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,10 @@
-const CACHE_VERSION = 1
+const CACHE_VERSION = 2
 const CURRENT_CACHES = { offline: `offline-v${CACHE_VERSION}` }
 const OFFLINE_URL = '/offline/index.html'
 let root
 
 const createCacheBustedRequest = url => {
-	let request = new Request(url, {
-		cache: 'reload'
-	})
+	let request = new Request(url, { cache: 'reload' })
 
 	if ('cache' in request) return request
 
@@ -21,23 +19,19 @@ self.addEventListener('install', event => {
 	event.waitUntil(
 		fetch(createCacheBustedRequest(OFFLINE_URL)).then(async response => {
 			const cache = await caches.open(CURRENT_CACHES.offline)
-			return cache.put(`${root}/${OFFLINE_URL}`, response)
+			return cache.put(OFFLINE_URL, response)
 		})
 	)
 })
 
 self.addEventListener('activate', event => {
-	let expectedCacheNames = Object.keys(CURRENT_CACHES).map(key => {
-		return CURRENT_CACHES[key]
-	})
+	let expectedCacheNames = Object.keys(CURRENT_CACHES).map(key => CURRENT_CACHES[key])
 
 	event.waitUntil(
 		caches.keys().then(cacheNames => {
 			return Promise.all(
 				cacheNames.map(cacheName => {
-					if (expectedCacheNames.indexOf(cacheName) === -1) {
-						return caches.delete(cacheName)
-					}
+					if (expectedCacheNames.indexOf(cacheName) === -1) return caches.delete(cacheName)
 				})
 			)
 		})
@@ -46,8 +40,6 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
 	if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
-		event.respondWith(
-			fetch(event.request).catch(() => caches.match(OFFLINE_URL))
-		)
+		event.respondWith(fetch(event.request).catch(() => caches.match(OFFLINE_URL)))
 	}
 })
